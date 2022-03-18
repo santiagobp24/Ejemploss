9790dc4736119dc26e5c240b6a6dc3e040d9d694
santiagobp24 <santiago83iti@gmail.com>
2022-03-17 21:09:35 -0500

    CoMIT5

---
+++ src/main/java/Ejemplos2/PrimeroV/commit5.java
@@ -0,0 +1,16 @@
+package Ejemplos2.PrimeroV;
+
+public class commit5 {
+	
+	public static void main (String[] args)
+	{
+		
+		
+		int carro = 9;
+		System.out.print(carro);
+		System.out.print(carro + carro*4);
+		
+		
+	}
+
+}
