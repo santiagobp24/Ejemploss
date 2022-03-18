e2aaa2afbf70ff507c5bdea8f63e2f2d8657fe13
santiagobp24 <santiago83iti@gmail.com>
2022-03-17 21:03:37 -0500

    new feature

---
+++ src/main/java/Ejemplos2/PrimeroV/commit4.java
@@ -0,0 +1,15 @@
+package Ejemplos2.PrimeroV;
+
+public class commit4 {
+	public static void main (String[] args)
+	{
+		
+		
+		int carro = 9;
+		System.out.print(carro);
+		System.out.print(carro + carro*3);
+		
+		
+	}
+
+}
