e61a7e685c1db834f75e6d2a6ea5dbb9f8664ac2
santiagobp24 <santiago83iti@gmail.com>
2022-03-17 20:37:54 -0500

    año

---
+++ src/main/java/Ejemplos2/PrimeroV/commit3.java
@@ -0,0 +1,15 @@
+package Ejemplos2.PrimeroV;
+
+public class commit3 {
+	public static void main (String[] args)
+	{
+		
+		
+		int carro = 9;
+		System.out.print(carro);
+		System.out.print(carro + carro*2);
+		
+		
+	}
+
+}
